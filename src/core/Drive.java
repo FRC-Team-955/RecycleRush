7068ebf43687d83831d85f14fae7375b1411bf1d
Merfoo <fauzimerfoo@gmail.com>
2015-01-11 15:33:23 -0800

    Added field centric

--- src/core/Drive.java
+++ src/core/Drive.java
@@ -1,6 +1,11 @@
 package core;
 
 import edu.wpi.first.wpilibj.Talon;
+import util.Config;
+import util.MyJoystick;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Gyro;
+import java.lang.Math;
 
 public class Drive {
 	Talon rightTalonOne;
@@ -12,6 +17,56 @@ public class Drive {
 	Talon frontTalon;
 	
 	Talon backTalon;
+	
+	MyJoystick joy;
+	
+	Gyro gyro;
+	
+	public Drive (MyJoystick newJoy, int port) {
+        joy = newJoy;
+        gyro = new Gyro(port);
+        gyro.reset();
+    }
+	
+	public void fieldCentric() {
+		//Moving with left joy
+		double joyX = joy.getLeftStickX();
+		double joyY = joy.getLeftStickY();
+		
+		double r = (Math.sqrt(joyX * joyX) + (joyY * joyY));
+		double a = Math.atan2(joyY, joyX);
+		double b = (getGyro() + (360 - a));
+		double finalTriAngle = b - getQuadrental(b);
+		double frontBackMotors = (r * Math.sin(finalTriAngle));
+		double leftRightMotors = (r * Config.Drive.speedMultiplier * Math.cos(finalTriAngle));
+		
+		setFrontBackSpeed(frontBackMotors);
+		setLeftRightSpeed(leftRightMotors);
+		
+		//Turning with right joy
+		double joyXPos = joy.getRightStickX();
+		
+		rightTalonOne.set(joyXPos);
+		rightTalonTwo.set(joyXPos);
+		
+		leftTalonOne.set(joyXPos);
+		leftTalonTwo.set(joyXPos);
+		
+		frontTalon.set(joyXPos);
+		backTalon.set(joyXPos);
+	}
+	public void setFrontBackSpeed(double speed) {
+		frontTalon.set(speed);
+		backTalon.set(speed);
+	}
+	
+	public void setLeftRightSpeed(double speed) {
+		rightTalonOne.set(speed);
+		rightTalonTwo.set(speed);
+		
+		leftTalonOne.set(speed);
+		leftTalonTwo.set(speed);
+	}
 	
 	public void setRightSpeed(double speed) {
 		rightTalonOne.set(speed);
@@ -59,5 +114,42 @@ public class Drive {
 		
 		leftTalonOne.set(-speed);
 		leftTalonTwo.set(-speed);
+	}
+	
+	public void setSpeed(double leftSpeed, double rightSpeed, double frontSpeed, double backSpeed) {
+		rightTalonOne.set(rightSpeed);
+		rightTalonTwo.set(rightSpeed);
+		
+		leftTalonOne.set(leftSpeed);
+		leftTalonTwo.set(leftSpeed);
+		
+		frontTalon.set(frontSpeed);		
+		backTalon.set(backSpeed);	
+	}
+	
+	public double getGyro() {
+		double finalAngle = (90 + (gyro.getAngle() - 360));
+		for(int i = 0; finalAngle > 360; i++) {
+			finalAngle -= 360;
+		}
+		return finalAngle;
+	}
+	
+	public int getQuadrental(double angle) {
+		if(angle <= 90 && angle > 0) {
+			return 0;
+		}
+		if(angle <= 180 && angle > 90) {
+			return 90;
+		}
+		if(angle <= 270 && angle > 180) {
+			return 180;
+		}
+		if(angle <= 360 && angle > 270) {
+			return 270;
+		}
+		//Stuff got messed up and doesn't work and we need to change stuff
+		System.out.println("Stuff got messed up and doesn't work and we need to change stuff");
+		return 0;
 	}
 }
--- src/util/Config.java
+++ src/util/Config.java
@@ -3,11 +3,17 @@ package util;
 public class Config {
 	
 	public class MyJoystick {
-	
+		public static final int chnLeftStickY = 10;
+		public static final int chnLeftStickX = 11;
+		public static final int chnRightStickY = 12;
+		public static final int chnRightStickX = 13;
+		
 	}
 	
 	public class Drive {
 		//Talon Channels
+		public static final int turningScale = 1;
+		
 		public static final int leftChanOne = 0;
 		public static final int leftChanTwo = 0;
 		
@@ -17,6 +23,8 @@ public class Config {
 		public static final int backChan = 0;
 		
 		public static final int frontChan = 0;
+		
+		public static final double speedMultiplier = 1;
 	}
 	
 	public class Intake {
