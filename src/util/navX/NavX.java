74aa32492f4a81375dd8846587727de380c55b0c
Aditya Sriram <adiram18@gmail.com>
2015-02-04 20:41:40 -0800

    Added cosmetic changes

--- src/util/navX/NavX.java
+++ src/util/navX/NavX.java
@@ -3,30 +3,19 @@ package util.navX;
 public class NavX extends IMUAdvanced {
 
 	double offsetAngle;
-	
-	
-	
-	public NavX(SerialPort serial_port, byte update_rate_hz, double offsetAngle){
-	
+		
+	public NavX(SerialPort serial_port, byte update_rate_hz, double offsetAngle)
+	{
 	super(serial_port, update_rate_hz);
 	this.offsetAngle = offsetAngle;
+		}
+
+
+
+public double getAngle()
+	{
 	
-	
-	
+	return (double )super.getYaw() +  offsetAngle;
+		
 	}
-
-
-
-public double getAngle(){
-	
-	return 
-			(double )super.getYaw() +  offsetAngle;
-	
-	
-	
-}
-
-
-
-
 }
